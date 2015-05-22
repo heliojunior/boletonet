6861a46b9b81322118d04b36ef519f0b39ff60dd
Hélio <helio@rofran.com.br>
2015-05-22 16:42:17 -0300

    adicionada classe que conterá extension méthods

---
+++ src/Boleto.Net/Util/ExtensionMethods.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoletoNet.Util
+{
+    public static class ExtensionMethods
+    {
+        public static string Limit(this string texto, int limit)
+        {
+            return texto.Length > limit ? texto.Substring(0, limit) : texto;
+        }
+    }
+}
